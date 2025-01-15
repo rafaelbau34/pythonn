d2327f83166f7672d69d0b02304fffe3ef712cbd
rafael <rafaelbaugar@gmail.com>
2025-01-15 11:46:07 -0600

    clase 15 de enero

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+#Definicion de dos variables en python para hacer calculos matematicos
+a = 10
+b = 5
+
+suma = a + b
+resta = a - b
+multi = a * b
+div = a * b
+
+print('El resultado de la suma es:', suma)
+print('El resultado de la resta es:', resta)
+print('El resultado de la multiplicación es:', multi)
+print('El resultado de la división es:', div)
