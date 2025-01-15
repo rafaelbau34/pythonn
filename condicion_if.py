e608c54f7df98fa014ae06d46842862a94575779
rafael <rafaelbaugar@gmail.com>
2025-01-15 11:46:54 -0600

    clase 15 enero

---
+++ condicion_if.py
@@ -0,0 +1,6 @@
+edad = 7
+
+if edad < 19:
+    print('Eres menor de edad')
+else:
+    print('Eres mayor de edad')
---
+++ condicion_usuario.py
@@ -0,0 +1,8 @@
+#ESTE EJERCICIO PIDE UN USUARIO Y CONTRASEÑA
+usuario = input('Ingrese su usuario:')
+contraseña = input('Ingrese su contraseña:')
+
+if usuario is 'admin' and contraseña is 'admin 123':
+    print('Bienvenido al sistema')
+else:
+    print('Usuario o contraseña incorrectos')
