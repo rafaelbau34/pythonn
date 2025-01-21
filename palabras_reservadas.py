ec38f1c0ed0236ae1ec81a8f8fea5f3cd0c28e36
rafael <rafaelbaugar@gmail.com>
2025-01-21 17:13:43 -0600

    ejercicios en clase 21/01/25

---
+++ palabras_reservadas.py
@@ -0,0 +1,49 @@
+"""
+Las palabras reservadas, son palabras exclusivas del
+lenguaje de programación Python, que tienen un significado especial
+y no pueden ser utilizadas como identificadores de variables, funciones
+"""
+
+# Lista de palabras reservadas
+palabras_reservadas = [
+    'False',
+    'None',
+    'True',
+    'and',
+    'as',
+    'assert',
+    'break',
+    'class',
+    'continue',
+    'def',
+    'del',
+    'elif',
+    'else',
+    'except',
+    'finally',
+    'for',
+    'from',
+    'global',
+    'if',
+    'import',
+    'in',
+    'is',
+    'lambda',
+    'nonlocal',
+    'not',
+    'or',
+    'pass',
+    'raise',
+    'return',
+    'try',
+    'while',
+    'with',
+    'yield'
+]
+
+nombre = 'Rafael'
+print(nombre)
+
+nombre_completo = nombre + ' Bautista'
+print(nombre_completo) 
+print(len(palabras_reservadas))
---
+++ uso_de_funciones.py
@@ -0,0 +1,17 @@
+#Ejercicio 1
+nombre = input('¿Cual es tu nombre? ')
+print('Hola', nombre)
+
+
+#Ejercicio 3
+def suma():
+    prim = input('Ingresa un número:')
+    seg = input('Ingresa otro número:')
+
+    num1 = int(prim)
+    num2 = int(seg)
+    resultado = num1 + num2
+    print('El resultado de la suma es:', resultado)
+
+suma()
+
